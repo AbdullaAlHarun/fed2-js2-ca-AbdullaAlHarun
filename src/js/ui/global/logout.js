b5b4c3121faa77c74265cd67c9f69641fb8f5a11
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-10-04 15:09:57 +0200

    Added delect functionality , logout functionality and added footer on all Apps !

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,1 +1,10 @@
-export function setLogoutListener() {}
+import {onLogout} from "../auth/logout.js";
+/**
+ * Attaches a click event listener to the logout button.
+ */
+export function setLogoutListener() {
+    const logoutBtn = document.getElementById('logoutBtn');
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', onLogout)
+    }
+}
